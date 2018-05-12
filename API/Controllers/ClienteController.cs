0c197e44747cc7124eb08d1a33b3c5711fa9df35
Vinicius Trindade <viniciustrin@live.com>
2018-05-12 12:25:13 -0300

    Add project files.

---
+++ API/Controllers/ClienteController.cs
@@ -0,0 +1,57 @@
+﻿using API.Models;
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+
+    [Authorize]
+    public class ClienteController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public ClienteController()
+        {
+            _context = new ApplicationDbContext();
+
+        }
+
+
+        public HttpResponseMessage Get()
+        {
+            try
+            {
+                var lista = _context.Clientes.ToList();
+                return Request.CreateResponse(HttpStatusCode.OK, lista);
+            }
+            catch (Exception ex)
+            {
+                HttpError err = new HttpError(ex.Message);
+                return Request.CreateResponse(HttpStatusCode.BadRequest, err);
+            }
+            
+        }
+
+
+        [HttpPost]
+        public HttpResponseMessage Post([FromBody] Cliente cli)
+        {
+            cli.DataCadastro = DateTime.Now;
+            try
+            {
+                _context.Clientes.Add(cli);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                HttpError err = new HttpError(ex.Message);
+                return Request.CreateResponse(HttpStatusCode.BadRequest,err);                
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, cli);
+        }
+
+    }
+}
---
+++ API/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+        [Required]
+        public DateTime DataCadastro { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+    }
+}
